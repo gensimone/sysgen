dc14b5bd3ca1594c25568811da620e6ca0af0088
Simone Gentili <gensimone.git@gmail.com>
2026-03-10 08:58:57 +0100

    Update paru.sh

--- arch.d/paru.sh
+++ arch.d/paru.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
-if ! sudo pacman -S --needed base-devel; then
-    echo "Aborted" >&2
-    exit 1
-fi
+PARU_PATH="$PWD/paru"
 
-if git clone https://aur.archlinux.org/paru.git; then
-    cd paru
-    makepkg -si
+if command -V paru >/dev/null 2>&1; then
+    echo >&2 "paru: Already installed."
 else
-    echo "Aborted" >&2
-    exit 1
+    if ! sudo pacman -S --needed base-devel; then
+        echo "paru: Aborted" >&2
+    else
+        if git clone https://aur.archlinux.org/paru.git "$PARU_PATH"; then
+            cd paru
+            makepkg -si
+            rm -rfv $PARU_PATH
+        else
+            echo "paru: Aborted" >&2
+        fi
+    fi
 fi
